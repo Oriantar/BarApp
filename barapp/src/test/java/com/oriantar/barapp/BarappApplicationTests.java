8b8bba7154d67ab3b40ac60ebcabed5af206584c
Tim Maurizio <tim.maurizio@gmail.com>
2025-04-20 14:56:04 +0200

    added springboot

---
+++ barapp/src/test/java/com/oriantar/barapp/BarappApplicationTests.java
@@ -0,0 +1,13 @@
+package com.oriantar.barapp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BarappApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
